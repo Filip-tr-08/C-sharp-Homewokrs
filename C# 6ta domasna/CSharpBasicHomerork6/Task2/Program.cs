86d6c9699243f644054033574ca23e8820e13f2e
Filip-tr-08 <filiptrajo@yahoo.com>
2021-04-18 20:48:05 +0200

    update

--- C# 6ta domasna/CSharpBasicHomerork6/Task2/Program.cs
+++ C# 6ta domasna/CSharpBasicHomerork6/Task2/Program.cs
@@ -8,26 +8,18 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
             List<double> numbers = new List<double>();
            for(int i = 0; i < 10; i++)
             {
-                startAgain:
-               
-                Console.WriteLine("Enter a number!");
-                bool success = double.TryParse(Console.ReadLine(), out double number);
-                if (success)
-                {
-                    numbers.Add(number);
-                }
-                else {
-                    Console.WriteLine("Wrong input try again");
-                    goto startAgain;
-                    
-                }
+                double x = 0;
+                x = Math.Round(rand.NextDouble()*100,2);
+                Console.WriteLine(x);
+                numbers.Add(x);
             }
 
-            List<double> newNumbers = (numbers.Select(n => n * n)).ToList();
-            foreach(int number in newNumbers)
+            List<double> newNumbers = (numbers.Select(n => Math.Round(Math.Pow(n,2.0),2))).ToList();
+            foreach(double number in newNumbers)
             {
                 Console.Write($" {number}");
             }
