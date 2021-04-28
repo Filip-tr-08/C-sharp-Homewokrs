263c0932738befe97a962f12c4fcc380745ffcd9
Filip-tr-08 <filiptrajo@yahoo.com>
2021-04-28 18:53:54 +0200

    update

--- CSharpAdvanceHomeworkThree/App/Program.cs
+++ CSharpAdvanceHomeworkThree/App/Program.cs
@@ -10,19 +10,10 @@ namespace App
         {
             foreach (Vehicle vehicle in VehicleDB.VehicleList)
             {
-                
-                    vehicle.PrintVehicle();
-               
+
+                vehicle.PrintVehicle();
+                Console.WriteLine($"The vehicle {vehicle.Type} is well implemented in the list : {Vallidator.Validate(vehicle)}");
             }
-            
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[0].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[0])}");
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[1].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[1])}");
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[2].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[2])}");
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[3].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[3])}");
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[4].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[4])}");
-            Console.WriteLine($"The vehicle {VehicleDB.VehicleList[5].Type} is well implemented in the list : {Vallidator.Validate(VehicleDB.VehicleList[5])}");
-
-
         }
     }
 }
